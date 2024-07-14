d307f09906ae0b1bf629517d5da79741a67857d7
Shivan Kathir <shivan.kathir@gmail.com>
2024-07-14 19:32:01 -0400

    did some styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { getInput } from "./weather.js";
+import { updateDisplay,getCityData } from "./interface.js";
+import "./style.css";
 
+async function displayLondon(){
+    const london = await getCityData("london");
+    updateDisplay(london);
+}
+
+displayLondon();
 getInput();
--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,12 @@
 async function getCityData(city){
     try{
-        const infoJSON = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=MF6FD9U2BF272ATQYZQ8LUC8V`,{mode:"cors"});
+        const infoJSON = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=MF6FD9U2BF272ATQYZQ8LUC8V`,{mode:"cors"});
         const info = await infoJSON.json();
     
         return info;
     }
     catch(err){
+        console.log(err);
         
 
     }
@@ -19,9 +20,30 @@ function updateDisplay(info){
     const newContent = document.createElement("div");
     newContent.classList.add("content");
 
+    const city = document.createElement("div");
+    city.textContent=info.resolvedAddress;
+    city.classList.add("city");
+
     const temp = document.createElement("div");
-    temp.textContent=info.currentConditions.temp;
+    temp.textContent=info.currentConditions.temp+" °C";
+    temp.classList.add("temp");
+
+    const feelsLike = document.createElement("div");
+    feelsLike.textContent="Feels like: "+info.currentConditions.feelslike +" °C";
+
+    const humidity = document.createElement("div");
+    humidity.textContent="Humidity: "+info.currentConditions.humidity +"%";
+
+    const wind = document.createElement("div");
+    wind.textContent = "Wind: "+info.currentConditions.windspeed +"km/h";
+
+
+    newContent.append(city);
     newContent.append(temp);
+    newContent.append(feelsLike);
+    newContent.append(humidity);
+    newContent.append(wind);
+    
     main.append(newContent);
 }
 
